e41dddb3868cb24eacf8eecbf899edc7268999a9
ItsPAnand <anandlatha7878@gmail.com>
2024-01-25 01:56:03 +0530

    Meme generator website for learnning react in interactive way

---
+++ README.md
@@ -0,0 +1,2 @@
+This is Meme Generator app 
+for learning react in more interactive way
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// import React from "react"
+// import "../index.css"
+import headerImage from "../images/troll-face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={headerImage} 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
